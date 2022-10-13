c440f27ad02936a7d52591e06e789b8321bf68de
Faduma Muse <faduma@fadumas-mbp.home>
2022-10-13 12:32:31 +0100

    Imports intro

--- opp_modules_packages.py
+++ opp_modules_packages.py
@@ -1,6 +1,19 @@
 # Lucky draw
-# Key word called input
+# Key word called import
+# Import random
 
-import random
+# print(random)
+#
+# print(random.random()) #Each time it's run it generates a random number
 
-print(random.random()) #Each time it's run it generates a random number
+
+# Calculate DOB or year of birth
+
+from random import *
+import math # compute any number - e.g round the figure up
+
+
+number = 23.66
+
+print(math.ceil(number)) #round up by using the method .ceil from the math import
+print(math.floor(number)) #round down by using the method .floor from the math import
