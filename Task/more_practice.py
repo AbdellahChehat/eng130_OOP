fc6af110b56c0f87932b40f31ede08e4eaca6263
Faduma Muse <faduma@fadumas-mbp.home>
2022-10-17 10:28:28 +0100

    another last min practice

--- Task/more_practice.py
+++ Task/more_practice.py
@@ -1,24 +1,40 @@
 
-def shopping_items():
+# def shopping_items():
+#
+#     shopping_items_list = {"eggs":1.85,
+#                            "bread": 1.50,
+#                            "peppers":0.90}
+#
+#     return shopping_items_list.values()
+#
+# print(sum(shopping_items()))
+# print(shopping_items())
+#
+#
+#
+# def shopping_items():
+#
+#     shopping_items_list = {"eggs":1.85,
+#                            "bread": 1.50,
+#                            "peppers":0.90}
+#
+#     return shopping_items_list.items()
+#
+#
+# print(shopping_items())
 
-    shopping_items_list = {"eggs":1.85,
-                           "bread": 1.50,
-                           "peppers":0.90}
 
-    return shopping_items_list.values()
-
-print(sum(shopping_items()))
-print(shopping_items())
+def password_length (password):
 
 
 
-def shopping_items():
+    if len(password) < 5:
+        return "Your Password is too short"
+    elif len(password) > 20:
+        return "Your password is too long"
+    else:
+        return "Your pass is an acceptable length"
 
-    shopping_items_list = {"eggs":1.85,
-                           "bread": 1.50,
-                           "peppers":0.90}
-
-    return shopping_items_list.items()
+print(password_length("sdsdddas"))
 
 
-print(shopping_items())
